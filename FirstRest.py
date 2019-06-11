0dc84d352ee1c5f444422818002d4fc9505cadc6
Dila <dila@vurung.com>
2019-06-11 10:21:09 +0545

    test

--- FirstRest.py
+++ FirstRest.py
@@ -2,14 +2,12 @@
 #https://pythonhow.com/using-virtualenv-with-your-flask-app/
 #https://www.youtube.com/watch?v=Us9DuF4KWUE
 #https://flask-cors.readthedocs.io/en/latest/  py charm https://www.youtube.com/watch?v=ZVGwqnjOKjk
+#https://stackoverflow.com/questions/19135867/what-is-pips-equivalent-of-npm-install-package-save-dev
 from flask import Flask,request,jsonify
 from flask_restful import Resource,Api
 from flask_cors import CORS,cross_origin
-
+from custom_cors import crossdomain
 app=Flask(__name__)
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
-app.config['CORS_HEADERS'] = 'Content-Type'
-cors = CORS(app, resources={"/api/*": {"origins": "*"}})
 
 """
 @app.route('/',methods=['GET','POST'])
@@ -25,15 +23,14 @@ def getMultiplication(num):
     return jsonify({'resule':num*10})
 """
 
-#cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api=Api(app)
 
 class ClassOne(Resource):
-    @cross_origin(origin='*', headers=['Content- Type', 'Authorization','Access-Control-Allow-Origin'])
+    @crossdomain(origin='*')
     def get(self):
         return {"key":"one"}
 
-    @cross_origin(origin='*', headers=['Content- Type', 'Authorization','Access-Control-Allow-Origin'])
+    @crossdomain(origin='*')
     def post(self):
         some_json=request.get_json()
         return {'key':'modified '+some_json},201
@@ -41,7 +38,6 @@ class ClassOne(Resource):
 class ClassTwo(Resource):
     def get(self,num):
         return {'result':num*20}
-
 
 api.add_resource(ClassOne,'/api/test')
 api.add_resource(ClassTwo,'/multi/<int:num>')
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # py-flask
 Introductory tutorial on flask (A Python Micro Framework)
+
+#npm install --save equivalent
+#pip install package  > requirements.txt
